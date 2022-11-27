6169c60c1f9b42da6d668eb6d2f3076ec19116d0
adiazc50 <30503952+adiazc50@users.noreply.github.com>
2022-11-27 05:05:01 -0500

    Update readme.ml

--- readme.ml
+++ readme.ml
@@ -1,2 +1,2 @@
-Aqui encontraras las carpetas del proyecto y el link del .exe
+Aqui encontraras el link de las carpetas del proyecto y el link del .exe
 https://drive.google.com/drive/folders/1IHd5dnl8ngXpoOW_d8glaAW8aT2JOrCD?usp=sharing
